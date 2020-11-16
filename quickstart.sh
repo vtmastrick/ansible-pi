b251d05ea8068b4ed21f8cf0b3354b17745fb894
vtmastrick <38921756+vtmastrick@users.noreply.github.com>
2020-11-16 14:35:35 -0500

    Removed sudo prefix

--- quickstart.sh
+++ quickstart.sh
@@ -1,9 +1,8 @@
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt autoremove -y
-sudo apt-get -y install ansible
-sudo useradd ansible
+apt-get update -y
+apt-get upgrade -y
+apt autoremove -y
+apt-get -y install ansible
+useradd ansible
 usermod -p "" ansible
-sudo usermod -a -G sudo ansible
-sudo mkhomedir_helper ansible
-
+usermod -a -G sudo ansible
+mkhomedir_helper ansible
