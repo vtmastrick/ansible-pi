b1fbe717df708bbc7a345f96bc42f96974c8717c
vtmastrick <38921756+vtmastrick@users.noreply.github.com>
2020-11-16 13:52:50 -0500

    Added quickstart script for clients to download

---
+++ quickstart.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt autoremove -y
+sudo apt-get -y install ansible
+sudo useradd ansible
+sudo passwd ansible
+sudo usermod -a -G sudo ansible
+sudo mkhomedir_helper ansible
