7c25a0bd41ab5e09a33c56dc16e02246169c594e
vtmastrick <38921756+vtmastrick@users.noreply.github.com>
2020-11-16 14:18:36 -0500

    Create default empty password.
    Password will be set on first ansible run

--- quickstart.sh
+++ quickstart.sh
@@ -3,6 +3,7 @@ sudo apt-get upgrade -y
 sudo apt autoremove -y
 sudo apt-get -y install ansible
 sudo useradd ansible
+usermod -p "" ansible
 sudo usermod -a -G sudo ansible
 sudo mkhomedir_helper ansible
-sudo passwd ansible
+
