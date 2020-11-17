7a7ed122a3582c231f498e06bb1f1532038e95bb
vtmastrick <38921756+vtmastrick@users.noreply.github.com>
2020-11-16 19:25:06 -0500

    Update account creation and SSH key download

--- quickstart.sh
+++ quickstart.sh
@@ -1,8 +1,22 @@
+  GNU nano 3.2                                                                             quickstart.sh                                                                              Modified
+
 apt-get update -y
 apt-get upgrade -y
 apt autoremove -y
 apt-get -y install ansible
+
+deluser --remove-home ansible
 useradd ansible
-usermod -p "" ansible
 usermod -a -G sudo ansible
 mkhomedir_helper ansible
+passwd ansible ""
+
+mkdir /home/ansible/.ssh
+touch /home/ansible/.ssh/authorized_keys
+
+rm id_rsa.pub
+wget https://raw.githubusercontent.com/vtmastrick/ansible-pi/master/setup/id_rsa.pub
+
+cat id_rsa.pub >> /home/ansible/.ssh/authorized_keys
+chmod 600 /home/ansible/.ssh/authorized_keys
+chmod 700 /home/ansible/.ssh
